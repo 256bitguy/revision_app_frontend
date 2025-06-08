549769db2e56933a42ac6383f85ad913a58e040f
256bitguy <ashishrajsri2233@gmail.com>
2025-06-08 17:41:41 +0530

    changed backend url

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:8000/api/v1',
-    baseURL: 'https://revision-app-0fia.onrender.com/api/v1',
+    baseURL: 'https://revision-app-1.onrender.com/api/v1',
 
 });
 
